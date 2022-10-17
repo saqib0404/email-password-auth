43423c4c62881e1c5f53190f722e266c08362d08
Saqib Ahmad <you@example.com>
2022-10-17 10:35:24 +0600

    login form added

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   ])
 
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 const login = () => {
     return (
         <div>
-             log
+            <Form className='w-50 mx-auto'>
+                <h2 className='text-primary'>Login Form</h2>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Log in
+                </Button>
+                <p className='my-4'>To Create an account, please <Link to='/register'>Register</Link></p>
+            </Form>
         </div>
     );
 };
